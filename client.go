3bcbc7f4ccff778211d6cf08f9ae162e0665b797
liuwenzhe <liuwenzhe_neoye@163.com>
2019-01-29 16:01:41 +0800

    fix: import error

--- client.go
+++ client.go
@@ -8,7 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	awss3 "github.com/shimohq/awos/aws"
-	osstorage "github.com/shimohq/awos/aws"
+	osstorage "github.com/shimohq/awos/oss"
 )
 
 type Client interface {
@@ -20,34 +20,34 @@ type Client interface {
 
 type Options struct {
 	storage string
-	oss *OSSOptions
-	aws *AWSOptions
+	oss     *OSSOptions
+	aws     *AWSOptions
 }
 
 type OSSOptions struct {
-	AccessKeyId string
+	AccessKeyId     string
 	AccessKeySecret string
-	Endpoint string
-	Bucket string
+	Endpoint        string
+	Bucket          string
 }
 
 type AWSOptions struct {
-	AccessKeyId string
+	AccessKeyId     string
 	AccessKeySecret string
-	Endpoint string
-	SSL bool
-	Region string
-	Bucket string
+	Endpoint        string
+	SSL             bool
+	Region          string
+	Bucket          string
 	// set S3ForcePathStyle = true when use minio
 	S3ForcePathStyle bool
 }
 
 const (
 	OSSStorage = "oss"
-	S3Storage = "aws"
+	S3Storage  = "aws"
 )
 
-func New(options *Options) (*Client, error){
+func New(options *Options) (*Client, error) {
 	var miossClient Client
 
 	if options.storage == OSSStorage {
@@ -75,16 +75,16 @@ func New(options *Options) (*Client, error){
 		// use minio
 		if awsConfig.S3ForcePathStyle == true {
 			sess = session.Must(session.NewSession(&aws.Config{
-				Region: aws.String(awsConfig.Region),
-				DisableSSL: aws.Bool(awsConfig.SSL == false),
-				Credentials: credentials.NewStaticCredentials(awsConfig.AccessKeyId, awsConfig.AccessKeySecret, ""),
-				Endpoint: aws.String(awsConfig.Endpoint),
+				Region:           aws.String(awsConfig.Region),
+				DisableSSL:       aws.Bool(awsConfig.SSL == false),
+				Credentials:      credentials.NewStaticCredentials(awsConfig.AccessKeyId, awsConfig.AccessKeySecret, ""),
+				Endpoint:         aws.String(awsConfig.Endpoint),
 				S3ForcePathStyle: aws.Bool(true),
 			}))
 		} else {
 			sess = session.Must(session.NewSession(&aws.Config{
-				Region: aws.String(awsConfig.Region),
-				DisableSSL: aws.Bool(awsConfig.SSL == false),
+				Region:      aws.String(awsConfig.Region),
+				DisableSSL:  aws.Bool(awsConfig.SSL == false),
 				Credentials: credentials.NewStaticCredentials(awsConfig.AccessKeyId, awsConfig.AccessKeySecret, ""),
 			}))
 		}
@@ -92,7 +92,7 @@ func New(options *Options) (*Client, error){
 		service := s3.New(sess)
 		awsClient := &awss3.AWS{
 			BucketName: awsConfig.Bucket,
-			Client: service,
+			Client:     service,
 		}
 		miossClient = awsClient
 
